0913575e8d9aa2bee75fc4cce103c13e143879e6
qkrwldnsdlek-rgb <qkrwldnsdlek@gmail.com>
2026-04-23 01:44:40 +0900

    add search feature

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.example.board.repository;
 import com.example.board.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
