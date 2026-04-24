128b3ca6eb184ad885742630a192705d10f5c664
qkrwldnsdlek-rgb <qkrwldnsdlek@gmail.com>
2026-04-24 17:36:31 +0900

    add admin api

--- src/main/java/com/example/board/controller/StatsController.java
+++ src/main/java/com/example/board/controller/StatsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/stats")
--- src/main/java/com/example/board/service/StatsService.java
+++ src/main/java/com/example/board/service/StatsService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
